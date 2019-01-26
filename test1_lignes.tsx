3c65c96e96e23234dfa24136c1faae3d38d721f1
Yoann Fleytoux <yoann.fleytoux@gmail.com>
2019-01-26 20:04:10 +0100

    Add files via upload

---
+++ test1_lignes.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="moza_lignes" tilewidth="64" tileheight="64" tilecount="32" columns="8">
+ <image source="moza_lignes.png" width="512" height="256"/>
+</tileset>
